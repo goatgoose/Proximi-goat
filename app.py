cd7070dddd517d0b3e5ae4401c68f357000e6c8a
goatgoose <samuel.clark@uvm.edu>
2020-12-11 23:17:01 -0500

    session view

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 import uuid
 
 app = Flask(__name__)
 
 
+@app.route("/call_example")
+def call_example():
+    return render_template("call_example.html", peer_id=str(uuid.uuid4()))
+
+
 @app.route("/")
 def home():
-    return render_template("home.html", peer_id=str(uuid.uuid4()))
+    return redirect("/session_view")
+
+
+@app.route("/session_view")
+def session_view():
+    temp_sessions = [
+        {
+            "name": "Test Session 1",
+            "players": 12
+        },
+        {
+            "name": "Test Session 2",
+            "players": 17
+        }
+    ]
+    return render_template("session_view.html", sessions=temp_sessions)
 
 
 if __name__ == '__main__':

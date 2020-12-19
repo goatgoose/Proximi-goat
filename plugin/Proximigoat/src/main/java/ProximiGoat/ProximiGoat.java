a7cd1f2e01e0f119b3e9b2d710701193666d102a
goatgoose <samuel.clark@uvm.edu>
2020-12-19 17:38:18 -0500

    log player location

--- plugin/Proximigoat/src/main/java/ProximiGoat/ProximiGoat.java
+++ plugin/Proximigoat/src/main/java/ProximiGoat/ProximiGoat.java
@@ -1,15 +1,33 @@
 package ProximiGoat;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class ProximiGoat extends JavaPlugin {
+public class ProximiGoat extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         getLogger().info("ProximiGoat enabled!");
+
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
         getLogger().info("ProximiGoat disabled!");
+    }
+
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+        Location location = event.getPlayer().getLocation();
+        Bukkit.broadcastMessage(player.getDisplayName() + ": " + location.toString());
     }
 }
